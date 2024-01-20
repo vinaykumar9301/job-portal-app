ba31282178dfe04752f5f8fde41913258336d194
VINAY_KUMAR_DEVOPS <109009399+vinaykumar9301@users.noreply.github.com>
2024-01-20 11:51:08 +0530

    Add files via upload
    Job portal app with Node.js, Express, MongoDB. Features bcryptjs, colors, cors, dotenv, and more for security. Explore jobs, manage applications, and benefit from clear API documentation with Swagger. Run with nodemon server.js for seamless development.

---
+++ server.js
@@ -0,0 +1,83 @@
+// API Documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
+// Packages imports
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
+// Security packages
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+// Files imports
+import connectDB from "./config/db.js";
+// Routes import
+import testRoutes from "./routes/testRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+import jobsRoutes from "./routes/jobsRoute.js";
+import userRoutes from "./routes/userRoutes.js";
+
+// Dot ENV config
+dotenv.config();
+
+// MongoDB connection
+connectDB();
+
+// Swagger API config
+// Swagger API options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Expressjs Job Portal Application",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+        // url: "https://nodejs-job-portal-app.onrender.com",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
+
+// Rest object
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/job", jobsRoutes);
+
+// Home route root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
+
+// Validation middleware
+app.use(errorMiddleware);
+
+// Port
+const PORT = process.env.PORT || 8080;
+
+// Listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
+    .bgCyan.white
+    );
+});
